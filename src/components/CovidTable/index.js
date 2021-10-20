4c124f5eb72e8232a2340601ca8d5b056dd730eb
luan <luandinh06@gmail.com>
2021-10-20 20:40:47 +0700

    done layout and elements

--- src/components/CovidTable/index.js
+++ src/components/CovidTable/index.js
@@ -1,12 +1,59 @@
 import React from 'react';
 import styles from './style.module.scss';
 
+import 'antd/dist/antd.css';
+import { DatePicker } from 'antd';
+const { MonthPicker, RangePicker, WeekPicker } = DatePicker;
+
 function CovidTable() {
     return (
-        <div>
-            <div className={styles.header}></div>
+        <div className={styles.covidTable}>
+            <div
+                className={styles.container}>
+                <div
+                    className={styles.header}>
+                    Covid Table
+                </div>
+                <div
+                    className={styles.chooseCountry}>
+                    <i class="fas fa-search"></i>
+                    <input type="text" placeholder='Country' />
+                </div>
+                <div className={styles.chooseDate}>
+                    <div className={styles.chooseDateElement}>
+                        <DatePicker
+                            onChange={onChange}
+                            format='DD/MM/YYYY'
+                            showToday='true'
+                            placeholder='From'
+                            size='large'
+                            className={styles.datePicker}
+                        />
+                    </div>
+                    <div className={styles.chooseDateElement}>
+                        <DatePicker
+                            onChange={onChange}
+                            format='DD/MM/YYYY'
+                            showToday='true'
+                            placeholder='To'
+                        />
+                    </div>
+                </div>
+                <div
+                    className={styles.result}
+                >
+                    <p className={styles.resultText}>{`Total Confirmed: `}</p>
+                    <p className={styles.resultText}>{`Total Deaths: `}</p>
+                    <p className={styles.resultText}>{`Total Recovered: `}</p>
+                </div>
+            </div>
+
         </div>
     );
+
+    function onChange(date, dateString) {
+        console.log(date, dateString);
+    }
 }
 
 export default CovidTable;
